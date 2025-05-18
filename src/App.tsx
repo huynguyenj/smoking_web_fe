3702f71cdad623fe4a12a421e6044a322f93c04d
huynguyenj <nguyenhuyjobs@gmail.com>
2025-05-18 09:21:46 +0700

    config tailwind theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
-import './App.css'
+import { Route , Routes } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    </>
+    <Routes>
+      <Route></Route>
+    </Routes>
   )
 }
 
