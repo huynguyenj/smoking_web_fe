dc5e61eded694ff98839e2bec586d0cd70b4307a
unknown <quangthse182541@fpt.edu.vn>
2025-05-20 20:20:48 +0700

    Login/Sign up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Route , Routes } from "react-router-dom"
-
+import LoginPage from "./pages/user/Auth/Login"
+import SignupPage from "./pages/user/Auth/Signup"
 function App() {
 
   return (
     
     <Routes>
-      <Route></Route>
+      <Route>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
+      </Route>
     </Routes>
   )
 }
