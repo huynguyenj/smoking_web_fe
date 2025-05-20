f7bbf0ec9372d12985dee9ec334a1e117663efe4
Long Lê <longfg333@gmail.com>
2025-05-20 07:28:30 +0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { Route , Routes } from "react-router-dom"
-import Footer from "./layouts/user_layout/Footer"
 
 function App() {
 
   return (
-    <Footer />
-    // <Routes>
-    //   <Route></Route>
-    // </Routes>
+    
+    <Routes>
+      <Route></Route>
+    </Routes>
   )
 }
 
